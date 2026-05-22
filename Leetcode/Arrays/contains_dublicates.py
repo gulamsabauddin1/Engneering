767dce9efc6b1c74894b6b04eeaee63bd6e2b834
Gulam Saba Uddin <gulamsabauddin1@gmail.com>
2026-05-22 16:10:35 +0530

    Leedcode problem 1

--- Leetcode/Arrays/contains_dublicates.py
+++ Leetcode/Arrays/contains_dublicates.py
@@ -1,2 +1,13 @@
-# Logic engine starting tomorrow!
+""""
+Q.no : 217. Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+"""
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(nums) != len(set(nums))
+        
+arr1=[1,2,3,3,4]
+print(Solution().containsDuplicate(arr1))

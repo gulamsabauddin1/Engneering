507c8d91a97473c3256d7e6b3be28dfa09ec192c
Gulam Saba Uddin <gulamsabauddin1@gmail.com>
2026-05-23 17:18:36 +0530

    FastAPI CRUD Application Practice

--- Development/WEB/main.py
+++ Development/WEB/main.py
@@ -1,20 +1,52 @@
 from fastapi import FastAPI
+from pydantic import BaseModel  
+from typing import List
 
 app = FastAPI()
 
+
+class Student_info(BaseModel):
+    pin:int
+    name:str
+    cource:str
+    
+#creating a list which stores the student information with datatypes defined in the Student_info class    
+student:List[Student_info] = []
+
+#get()=> to show data sent from server on website
+#post()=> to send data from website to server
+#put()=> to update data on server from website
+#delete()=> to delete data on server from website 
+
+
+
 @app.get("/")
-def home():
-    return {"message": "Welcome to your backend engine, Gulam! 😄"}
+def read_root():
+    return{"Welcome":"Welcome to FastAPI!"}
 
-@app.get("/items")
-def get_items():
-    return ["Item A", "Item B", "Item C"]
+@app.get("/students")
+def read_students():
+    return student
 
-@app.get("/username")
-def calculate():
-    print("Calculating username...")
-    return("A=10, B=20 ",10+20)
-    
+@app.post("/students")
+def add_student(input:Student_info):
+    student.append(input)
+    return student
+    print("Student added successfully!")
+    return {"message":"Student added successfully!"}
 
-   
-    
+@app.put("/students/{pin}")
+def update_student(pin:int, input:Student_info):
+    for i in range(len(student)):
+        if student[i].pin == pin:
+            student[i] = input
+            return {"message":"Student updated successfully!"}
+    return {"message":"Student not found!"}
+
+@app.delete("/students/{pin}")
+def delete_student(pin:int):
+    for i in range(len(student)):
+        if student[i].pin == pin:
+            student.pop(i)
+            return{"message":"Student deleted successfully!"}
+    return {"message":"Student not found!"}
